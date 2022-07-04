80bbfa634534c082dea909f64493023b42b87335
BartelKarlijn <legoreus@gmail.com>
2022-07-04 18:36:25 +0200

    Testing all pins via pwm

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,119 @@
-#include <main\include.h>
+#include <Arduino.h>
+
+#define TFT_MOSI 23 // In some display driver board, it might be written as "SDA" and so on.
+#define TFT_SCLK 18
+#define TFT_DC   21 // Data Command control pin
+#define TFT_RST  22 // Reset pin (could connect to Arduino RESET pin)
+#define TFT__BL  19    //Backlight
+
+#define TFT0_CS  32    //Chip Select pin tftx
+#define TFT1_CS  33    //Chip Select pin tftx
+#define TFT2_CS  25    //Chip Select pin tftx
+#define TFT3_CS  26    //Chip Select pin tftx
+#define TFT4_CS  27    //Chip Select pin tftx
+#define TFT5_CS  13    //Chip Select pin tftx
+
+///////////////// TFT backlight  //////////////////////////////
+#define PWMFreq 5000
+#define PWMChannel 0
+#define PWMResolution 8
+#define LEDSTARTBLINK 4  // How many blinks at start?
 
 // Created by Bodmer 17/3/20 as an example to the TFT_eSPI library:
 // https://github.com/Bodmer/TFT_eSPI
 
-void plottest() {
-  for (int i = 0; i <= 5; i++ ) {
-    digitalWrite( tft_cs[i], LOW);
-    needle.pushRotated( 90, COLOR_TRANSP);
-    digitalWrite( tft_cs[i], HIGH);
-}
+void led(int pin) {
+  ledcDetachPin(TFT_MOSI);
+  ledcDetachPin(TFT_SCLK);
+  ledcDetachPin(TFT_DC  );
+  ledcDetachPin(TFT_RST );
+  ledcDetachPin(TFT__BL );
+  ledcDetachPin(TFT0_CS );
+  ledcDetachPin(TFT0_CS );
+  ledcDetachPin(TFT1_CS );
+  ledcDetachPin(TFT2_CS );
+  ledcDetachPin(TFT3_CS );
+  ledcDetachPin(TFT4_CS );
+  ledcDetachPin(TFT5_CS );
+
+  ledcAttachPin(pin , PWMChannel);
 }
 
 // =======================================================================================
 // Setup
 // =======================================================================================
-void setup()   {
-  setupSerial();
-
-  // Create the Sprites
-  createNeedle();
-  createAxis();
-  createClear1();
-  createClear2();
-  createClear3();
-  createClear4();
-  // tft
-  setupTft();
-
-  moveFront = 4;
-  moveBack  = 2;
-
-  // Backlight
-  backlight = 50;
-  pinMode(TFT__BL, OUTPUT);
-  digitalWrite(TFT__BL, HIGH);
-  //ledcSetup(PWMChannel, PWMFreq, PWMResolution);
-  //ledcAttachPin(TFT__BL, PWMChannel);
-  //ledcWrite(PWMChannel, backlight);
-
-  myTime = millis();
-  SetupFinished();
+void setup() {
+  Serial.begin(115200);
+  Serial.println("beginnen") ; 
+  
+  // configure LED PWM functionalitites
+  ledcSetup(PWMChannel, PWMFreq, PWMResolution);
+  
+  // attach the channel to the GPIO to be controlled
+  Serial.println("We zijn er klaar voor") ; 
+  Serial.println("Mosi sClk, Dc, Rst, Bl, cs0-6");
 }
+
 
 // =======================================================================================
 // Loop
 // =======================================================================================
 void loop() {
-  unsigned long myTimeRef;
-  
-  // Plot needle at random angle 
-  for (int i = 0; i <= 5; i++ ) {
-    plottest();
-    //plotNeedle(angleBack[i], moveBack, angleFront[i], moveFront, tft_cs[i]);
-    angleBack[i] = angleBack[i] + moveBack;
-    if (angleBack[i] >= 360) {angleBack[i] = 0; }
-    angleFront[i] = angleFront[i] + moveFront;
-    if (angleFront[i] >= 360) {angleFront[i] = 0; }
+  if (Serial.available() > 0) {
+    // read the incoming string:
+    String incomingString = Serial.readString();
+
+    // prints the received data
+    Serial.print("I received: ");
+    Serial.println(incomingString);
+    switch (toupper(incomingString[0]))
+    {
+    case 'M':
+      led(TFT_MOSI);
+      break;
+    case 'C':
+      led(TFT_SCLK);
+      break;
+    case 'D':
+      led(TFT_DC);
+      break;
+    case 'R':
+      led(TFT_RST);
+      break;
+    case 'B':
+      led(TFT__BL);
+      break;
+    case '0':
+      led(TFT0_CS);
+      break;
+    case '1':
+      led(TFT1_CS);
+      break;
+    case '2':
+      led(TFT2_CS);
+      break;
+    case '3':
+      led(TFT3_CS);
+      break;
+    case '4':
+      led(TFT4_CS);
+      break;
+    case '5':
+      led(TFT5_CS);
+      break;
+    
+    default:
+      break;
+    }  
   }
 
-  // timing
-  cnt++;
-  //Serial.println(cnt);
-  if ( cnt >= 90 ) {
-    myTimeRef = millis() - myTime;
-    Serial.print(cnt);
-    Serial.print(" screens in ");
-    Serial.print(myTimeRef);
-    Serial.print(" ms = ");
-    Serial.print(cnt * 1000/ myTimeRef);
-    Serial.println("fps");
-    tft.fillScreen(COLOR_BACKGROUND);
-    moveFront++;
-    backlight = backlight + 50;
-    if (backlight > 255) {
-      backlight = 0;
-//      ledcWrite(PWMChannel, backlight);
-      }
-    Serial.print("Backlight ");
-    Serial.println(backlight);
-
-    if (moveFront >= 5) { 
-      moveFront = 1; 
-      moveBack++;
-      if (moveBack >= 5) { moveBack = 1;}
+  ledcWrite(PWMChannel, 0);
+  delay(3000);
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(PWMChannel, dutyCycle);
+    delay(15);
     }
-    myTime = millis();
-    cnt = 0;
-  }
-
+  ledcWrite(PWMChannel, 255);
+  delay(3000);
 }
