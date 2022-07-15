cc5a45304548b7e85dafc1df8087bed056b2b4f5
BartelKarlijn <legoreus@gmail.com>
2022-07-15 17:27:00 +0200

    eerste keer dat wifi werkt

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,13 @@ void plottest() {
 void setup()   {
   setupSerial();
   setupBacklight();
+  get_datafrom_eeprom (); // parameters uit eeprom halen (oa ssid/pw en PID)
+
+  //wifi
+  setup_ConnectKnownWifi();  // Aan gekende wifi connecteren
+  setup_AsyncWebserver();    // webserver om html te tonen 
+  setup_CreateAccessPoint(); // Toch eigen AccessPoint opzetten als gekende wifi niet gelukt.
+
 
   // Create the Sprites
   createNeedle();
