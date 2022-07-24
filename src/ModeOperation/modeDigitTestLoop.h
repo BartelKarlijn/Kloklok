a074c09f37e401cb00d6c83759b6a9247d65e90b
BartelKarlijn <legoreus@gmail.com>
2022-07-24 17:27:37 +0200

    dance finished weglaten in print

--- src/ModeOperation/modeDigitTestLoop.h
+++ src/ModeOperation/modeDigitTestLoop.h
@@ -2,16 +2,13 @@ void modeDigitTestLoop(){
   checkStillSameMode ();
   uint16_t nextNr = currentNr + 1;
   if (nextNr > 9) {nextNr = 0;}
-    showDigit(currentNr);
-    Print("Showing digit ");
-    Println(String(currentNr));
-    delay(10);
-
     calculateMovementToNr(nextNr);
 
     moveNextDigit();
-    Println("Dance finished");
     currentNr = nextNr;
+    showDigit(currentNr);
+    Print("Showing digit ");
+    Println(String(currentNr));
 
     delay(2000);
 }
