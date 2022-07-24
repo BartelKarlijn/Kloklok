34600dd7a93d02b1b644458dcbfaee90592fd7b6
BartelKarlijn <legoreus@gmail.com>
2022-07-24 17:22:45 +0200

    print statement corrigeren

--- src/ModeOperation/modeDigitTestLoop.h
+++ src/ModeOperation/modeDigitTestLoop.h
@@ -3,7 +3,8 @@ void modeDigitTestLoop(){
   uint16_t nextNr = currentNr + 1;
   if (nextNr > 9) {nextNr = 0;}
     showDigit(currentNr);
-    Println("Showing digit currentNr");
+    Print("Showing digit ");
+    Println(String(currentNr));
     delay(10);
 
     calculateMovementToNr(nextNr);
