d0d66df72a8266ef43e5c5bd7390e444ab95d31f
BartelKarlijn <legoreus@gmail.com>
2022-07-24 17:27:45 +0200

    read input

--- src/ModeOperation/modeSlaveLoop.h
+++ src/ModeOperation/modeSlaveLoop.h
@@ -1,4 +1,13 @@
 void modeSlaveLoop(){
+  String incomingString;
   checkStillSameMode ();
+  
+  if (Serial.available() > 0) {
+    // read the incoming string:
+    incomingString = Serial.readString();
 
+    // prints the received data
+    Print("I received: ");
+    Println(incomingString);
+  }
 }
