ccc7e5fb64391603b45d9fdbd4ab1a61c8132bb6
BartelKarlijn <legoreus@gmail.com>
2022-07-25 20:36:00 +0200

    demo aanpassen

--- src/ModeOperation/modeClockDemoSetup.h
+++ src/ModeOperation/modeClockDemoSetup.h
@@ -1,14 +1,14 @@
 void modeClockDemoSetup(){
+  currentNr = 0;
   startWifi();
   clearScreens(COLOR_BACKGROUND, true);
-  currentNr = 0;
-  Println("Showing test color with 0");
   Println("Give slaves time to catch up");
   delay(5000);
   distributeCommand(COMMAND_CLEARS, 1, 1, 1);  // clear screens reploting axis
   delay(1000);
   distributeCommand(COMMAND_SHOWDG, currentNr, currentNr, currentNr);  // Showing 0 on all screens
   showDigit(currentNr);
+  Println("Showing test color with 0");
   delay(2000);
   Println("Now in mode ClockDemo");
   delay(100);  
