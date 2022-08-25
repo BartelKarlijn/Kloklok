44e09cae9ef7c6dd5ac67bc0925c75711346f611
BartelKarlijn <legoreus@gmail.com>
2022-08-25 19:57:28 +0200

    webserial commandos nu ook goed interpreteren

--- src/main/readSerial.h
+++ src/main/readSerial.h
@@ -1,4 +1,5 @@
 void readSerial(){
+  String cmdString1, cmdString4;
   //Serial message
   if (Serial.available() > 0) {
     // read the incoming string:
@@ -12,12 +13,17 @@ void readSerial(){
   //Webserial
   if (flagWifiOn) {
     WebSerial.msgCallback(recvMsg);
-    if (     (incomingMessage.charAt(0) == CMD_START )
-         and (incomingMessage.charAt(4) == CMD_STOP  ) ){
+    if (incomingMessage != "" ) {
+      //check if valid command
+      cmdString1 = incomingMessage[0];
+      cmdString4 = incomingMessage[4];
+    }
+    if (  (cmdString1 == String(CMD_START) )
+      and (cmdString4 == String(CMD_STOP ) ) ){
       // only process valid commands
       messageChanged = true;
-      serialMessage = incomingMessage;
-      incomingMessage = "";
+      serialMessage = incomingMessage.substring(1, 4);
     }
+    incomingMessage = "";
   }
 }
