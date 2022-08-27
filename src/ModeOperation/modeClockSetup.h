6735e8b3d7c820536bd5de5ab9318e6141e6ee11
BartelKarlijn <legoreus@gmail.com>
2022-08-27 12:56:07 +0200

    digitdance remove currentnr

--- src/ModeOperation/modeClockSetup.h
+++ src/ModeOperation/modeClockSetup.h
@@ -3,8 +3,9 @@ void modeClockSetup(){
   clearScreens(COLOR_TEST, true);
   Print("Give slaves time to catch up");
   delay(DELAY_CATCHUP);
-  distributeCommand(COMMAND_SHOWDG, currentNr, currentNr, currentNr);  // Showing 0 on all screens
-  showDigit(currentNr, true);
+  time_X000 = 0;
+  distributeCommand(COMMAND_SHOWDG, time_X000, time_X000, time_X000);  // Showing 0 on all screens
+  showDigit(time_X000, true);
   Println("Showing test color with 0");
 
   // Init and get the time
